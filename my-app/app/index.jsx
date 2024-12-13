e65e6b7c63ade81ab773b68bf179e089cd583727
Tomba-Hopkins <saputrazamharimirza@gmail.com>
2024-12-13 21:01:40 +0700

    add: font balu

--- my-app/app/index.jsx
+++ my-app/app/index.jsx
@@ -5,9 +5,9 @@ import { Link } from "expo-router";
 const RootLayout = () => {
   return (
     <View className="flex-1 items-center bg-slate-900 justify-center ">
-      <Text className="text-2xl text-slate-200 ">Hello smith</Text>
+      <Text className="text-2xl text-slate-200 font-rwbold">Hello Pak</Text>
       <StatusBar style="auto" />
-      <Link href="/profile" className="text-slate-400">
+      <Link href="/profile" className="text-red-400">
         Go to Profile
       </Link>
     </View>
