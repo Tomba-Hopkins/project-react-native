adab7c8f1d85a0f35b626e7b3b6001a576f75c10
Tomba-Hopkins <saputrazamharimirza@gmail.com>
2024-12-19 09:00:54 +0700

    add: ternyata issue di config tailwind nya, ga ngarah ke ./components tapi ./app

--- my-app/app/index.jsx
+++ my-app/app/index.jsx
@@ -44,3 +44,4 @@ const RootLayout = () => {
 // });
 
 export default RootLayout;
+
--- my-app/components/CustomButton.jsx
+++ my-app/components/CustomButton.jsx
@@ -3,9 +3,9 @@ import React from 'react'
 
 const CustomButton = ({ title, handlePress, containerStyles, textStyles, isLoading }) => {
     return (
-        <TouchableOpacity className={`bg-secondary rounded-xl min-h-[62px] justify-center items-center`}>
-            <Text className="text-primary font-rwsemibold text-lg">CustomButton</Text>
-        </TouchableOpacity>
+        <TouchableOpacity disabled={isLoading} onPress={handlePress} activeOpacity={0.7} className={`bg-secondary rounded-xl min-h-[62px] justify-center items-center ${containerStyles} ${isLoading ? 'opacity-50' : ''}`}>
+            <Text className={`text-primary font-rwsemibold text-lg ${textStyles}`}>{title}</Text>
+        </TouchableOpacity >
     )
 }
 
