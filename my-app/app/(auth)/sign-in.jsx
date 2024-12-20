f653be73e9f0df2eb036e3b7e74d2899aa6658f8
Tomba-Hopkins <saputrazamharimirza@gmail.com>
2024-12-20 10:04:38 +0700

    add: ada

--- my-app/app/(auth)/sign-in.jsx
+++ my-app/app/(auth)/sign-in.jsx
@@ -1,11 +1,16 @@
-import { View, Text } from "react-native";
+import { View, Text, ScrollView } from "react-native";
 import React from "react";
+import { SafeAreaView } from "react-native-safe-area-context";
 
 const SignIn = () => {
   return (
-    <View>
-      <Text>SignIn</Text>
-    </View>
+    <SafeAreaView className="bg-primary h-full">
+      <ScrollView>
+        <View className="w-full justify-center min-h-[85vh] px-4 my-6">
+
+        </View>
+      </ScrollView>
+    </SafeAreaView>
   );
 };
 
--- my-app/app/index.jsx
+++ my-app/app/index.jsx
@@ -1,14 +1,16 @@
 import { View, StyleSheet, Text, ScrollView, Image } from "react-native";
 import { image } from "../constants";
+import { Redirect, router } from 'expo-router'
 
 import { SafeAreaView } from 'react-native-safe-area-context'
 import CustomButton from "../components/CustomButton.jsx";
+import { StatusBar } from "expo-status-bar";
 
 const RootLayout = () => {
   return (
     <SafeAreaView className="bg-primary h-full">
       <ScrollView contentContainerStyle={{ height: '100%', flexGrow: 1 }}>
-        <View className="w-full h-full justify-center items-center px-4">
+        <View className="w-full min-h-[85vh] justify-center items-center px-4">
           <Image source={image.logo} resizeMode="contain" className="w-[130px] h-[84px]" />
           <Image source={image.cards} className="max-w-[380px] w-full h-[300px]" resizeMode="contain" />
           <View className="relative mt-5">
@@ -27,9 +29,12 @@ const RootLayout = () => {
             embark on a journey of limitless exploration with Aora
           </Text>
 
-          <CustomButton title="Continue with Email" handlePress={() => { }} containerStyles="w-full mt-7" />
+          <CustomButton title="Continue with Email" handlePress={() => router.push('/sign-in')} containerStyles="w-full mt-7" />
         </View>
       </ScrollView>
+
+      {/* biar muncul status bar */}
+      <StatusBar backgroundColor="#161622" style="light" />
     </SafeAreaView>
   );
 };
