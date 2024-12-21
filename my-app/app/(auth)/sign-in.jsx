331bdaf833c4138c43d5261c4f8e027cb8c01e98
Tomba-Hopkins <saputrazamharimirza@gmail.com>
2024-12-21 10:16:26 +0700

    add: ada yang baru

--- my-app/app/(auth)/sign-in.jsx
+++ my-app/app/(auth)/sign-in.jsx
@@ -1,13 +1,29 @@
-import { View, Text, ScrollView } from "react-native";
+import { View, Text, ScrollView, Image } from "react-native";
 import React from "react";
 import { SafeAreaView } from "react-native-safe-area-context";
 
+
+import { image } from "../../constants";
+import FormField from "../../components/FormField";
+
 const SignIn = () => {
+
+  const [form, setForm] = useState({
+    email: '',
+    password: ''
+  })
+
+
   return (
     <SafeAreaView className="bg-primary h-full">
       <ScrollView>
-        <View className="w-full justify-center min-h-[85vh] px-4 my-6">
-
+        <View className="w-full justify-center h-full px-4 my-6">
+          <Image source={image.logo} resizeMode="contain" className="w-[115px] h-[35px]" />
+          <Text className="text-2xl text-white font-semibold mt-10  font-rwsemibold">Log in to Aora</Text>
+          <FormField title="Email" value={form.email} handleChangeText={(e) => setForm({
+            ...form,
+            email: e
+          })} otherStyles="mt-7" keyboardType="email-address" />
         </View>
       </ScrollView>
     </SafeAreaView>
---
+++ my-app/components/FormField.jsx
@@ -0,0 +1,14 @@
+import { View, Text } from 'react-native'
+import React, { useState } from 'react'
+
+const FormField = () => {
+
+
+    return (
+        <View className="space-y-2">
+            <Text className="text-base text-gray-100 font-rwmedium">FormField</Text>
+        </View>
+    )
+}
+
+export default FormField
